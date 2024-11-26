0bda6f01c5df47fee9b946ca65d0c33b8e7c1ded
Jaden <jaden.anthony2@gmail.com>
2024-11-26 12:35:36 -0500

    Updated Complete.java

--- Fluffy's Sweet Treats/src/Complete.java
+++ Fluffy's Sweet Treats/src/Complete.java
@@ -1,13 +1,27 @@
-//Author:
-//Last Modified:
+//Author: Tara-Lee Donald
+//Last Modified: 26-11-2024
 
-public class Complete extends Order{
+//Template of a completed order for archive purposes
 
-    public Complete(){
+public class Complete extends Order {
 
-        //Complete subclass of order
+    protected String dateCompleted;
+    protected String dueDate;
 
-        
+    public Complete(int id, Customer customer, String creationDate, String event, String flavour, String desc, float price, String deliveryLocation, String paymentStatus, String dueDate, String dateCompleted) {
+        super(id, customer, creationDate, event, flavour, desc, price, deliveryLocation, paymentStatus);
+    
+        this.dateCompleted = dateCompleted;
+        this.dueDate = dueDate;
+       
     }
- 
+
+    public String getDateCompleted(){
+        return dateCompleted;
+    }
+
+    public String getDueDate(){
+        return dueDate;
+    }
+
 }
