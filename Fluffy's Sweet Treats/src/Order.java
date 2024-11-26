8b31dccf3c89c9627af0682b8c312a62bb3e1957
Jaden <jaden.anthony2@gmail.com>
2024-11-26 12:40:40 -0500

    Updated Order.java

--- Fluffy's Sweet Treats/src/Order.java
+++ Fluffy's Sweet Treats/src/Order.java
@@ -1,5 +1,5 @@
 //Author: Tara-Lee Donald
-//Last Modified: 06-11-2024
+//Last Modified: 26-11-2024
 
 //Provides the original template of an order
 
@@ -9,7 +9,6 @@ public class Order{
     protected String desc;
     protected String deliveryLocation;
     protected String creationDate;
-    protected String dueDate;
     protected String paymentStatus;
     protected String flavour;
     protected Customer customer;
@@ -17,7 +16,7 @@ public class Order{
     protected int id;
 
     //Constructor accepts the id assigned to the customer, the customer's name, the customer's phone number, the date on which the entry was entered, the due date of the order, the type of cake, the description of the order, the cost of the cake, payment Status of the customer, the delivery location and the status of the progress of the order.
-    public Order(int id, Customer customer, String creationDate,  String event, String flavour, String desc, float price, String deliveryLocation, String paymentStatus, String dueDate){
+    public Order(int id, Customer customer, String creationDate,  String event, String flavour, String desc, float price, String deliveryLocation, String paymentStatus){
         this.id = id;
         this.customer = customer;
         this.event = event;
@@ -26,7 +25,6 @@ public class Order{
         this.price = price;
         this.deliveryLocation = deliveryLocation;
         this.creationDate = creationDate;
-        this.dueDate = dueDate;
         this.paymentStatus = paymentStatus;
 
     }
@@ -38,10 +36,6 @@ public class Order{
 
     public Customer getCustomer(){
         return customer;
-    }
-
-    public String getDueDate(){
-        return dueDate;
     }
 
     public String getEvent(){
