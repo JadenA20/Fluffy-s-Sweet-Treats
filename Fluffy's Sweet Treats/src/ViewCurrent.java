6452c7aa8fa3e7c5499346796f229a6f275e831e
Tara_Donald <taralee.kd.db@gmail.com>
2024-11-27 21:23:09 -0500

    Added required sorts

--- Fluffy's Sweet Treats/src/ViewCurrent.java
+++ Fluffy's Sweet Treats/src/ViewCurrent.java
@@ -11,13 +11,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class ViewCurrent extends JFrame{
 
-    private JPanel mainPanel, optionPanel, displayPanel;
-    private JButton create, edit, delete, export, exit;
+    private JPanel mainPanel, optionPanel, displayPanel, sortPanel;
+    private JButton create, edit, delete, export, exit, sortDueDate, sortPaymentState, sortCreationDate;
     private JLabel title, backgrounds;
     private JScrollPane scrollPane;
     private JTable table;
@@ -51,6 +54,9 @@ public class ViewCurrent extends JFrame{
         optionPanel = new JPanel();
         optionPanel.setLayout(new FlowLayout());
 
+        sortPanel = new JPanel();
+        sortPanel.setLayout(new FlowLayout());
+
         displayPanel = new JPanel();
         
 
@@ -66,38 +72,61 @@ public class ViewCurrent extends JFrame{
         create.setBackground(new Color(120, 67, 59));
         create.setForeground(new Color(255, 255, 255));
         create.setFont(h);
-        create.setBounds(30, 200, 100, 35);
+        create.setBounds(30, 200, 50, 35);
+
+        sortCreationDate = new JButton("Sort By Creation Date");
+        sortCreationDate.setBackground(new Color(120, 67, 59));
+        sortCreationDate.setForeground(new Color(255, 255, 255));
+        sortCreationDate.setFont(h);
+        sortCreationDate.setBounds(30, 200, 75, 35);
+
+        sortDueDate = new JButton("Sort by Due Date");
+        sortDueDate.setBackground(new Color(120, 67, 59));
+        sortDueDate.setForeground(new Color(255, 255, 255));
+        sortDueDate.setFont(h);
+        sortDueDate.setBounds(30, 200, 75, 35);
+
+        sortPaymentState = new JButton("Sort By Payment Status");
+        sortPaymentState.setBackground(new Color(120, 67, 59));
+        sortPaymentState.setForeground(new Color(255, 255, 255));
+        sortPaymentState.setFont(h);
+        sortPaymentState.setBounds(30, 200, 75, 35);
+
 
         edit = new JButton("Edit");
         edit.setBackground(new Color(120, 67, 59));
         edit.setForeground(new Color(255, 255, 255));
         edit.setFont(h);
-        edit.setBounds(70, 200, 100, 35);
+        edit.setBounds(70, 200, 50, 35);
 
         delete = new JButton("Delete");
         delete.setBackground(new Color(120, 67, 59));
         delete.setForeground(new Color(255, 255, 255));
         delete.setFont(h);
-        delete.setBounds(110, 200, 100, 35);
+        delete.setBounds(110, 200, 50, 35);
 
         export = new JButton("Export");
         export.setBackground(new Color(120, 67, 59));
         export.setForeground(new Color(255, 255, 255));
         export.setFont(h);
-        export.setBounds(150, 200, 100, 35);
+        export.setBounds(150, 200, 50, 35);
 
         exit = new JButton("Exit");
         exit.setBackground(new Color(120, 67, 59));
         exit.setForeground(new Color(255, 255, 255));
         exit.setFont(h);
-        exit.setBounds(190, 200, 100, 35);
+        exit.setBounds(190, 200, 50, 35);
 
 
         //Adding to MainPanel
         mainPanel.add(title);
         mainPanel.add(displayPanel);
+        mainPanel.add(sortPanel);
         mainPanel.add(optionPanel);
         optionPanel.add(create);
+        sortPanel.add(sortDueDate);
+        sortPanel.add(sortCreationDate);
+        sortPanel.add(sortPaymentState);
         optionPanel.add(edit);
         optionPanel.add(delete);
         optionPanel.add(export);
@@ -106,6 +135,9 @@ public class ViewCurrent extends JFrame{
 
         //Add Listeners
         create.addActionListener(new ButtonListener());
+        sortCreationDate.addActionListener(new ButtonListener());
+        sortDueDate.addActionListener(new ButtonListener());
+        sortPaymentState.addActionListener(new ButtonListener());
         edit.addActionListener(new ButtonListener());
         delete.addActionListener(new ButtonListener());
         export.addActionListener(new ButtonListener());
@@ -259,10 +291,47 @@ public class ViewCurrent extends JFrame{
             }
             if(e.getSource() == create){
                 // Code to implement
-                CreateOrders creOrd = new CreateOrders();
                 model.setRowCount(0);
+                CreateOrders creOrd = new CreateOrders();
+                
                 displayTable(orderList);
                 
+            }
+
+            if(e.getSource() == sortCreationDate){
+                model.setRowCount(0);
+                Collections.sort(orderList, new Comparator<Current>(){
+
+                    public int compare(Current o1, Current o2) {
+                        return o1.getCreationDate().compareTo(o2.getCreationDate());
+                    }
+                });
+               
+                displayTable(orderList);
+            }
+
+            if(e.getSource() == sortDueDate){
+                model.setRowCount(0);
+                Collections.sort(orderList, new Comparator<Current>(){
+
+                    public int compare(Current o1, Current o2) {
+                        return o1.getDueDate().compareTo(o2.getDueDate());
+                    }
+                });
+               
+                displayTable(orderList);
+            }
+
+            if(e.getSource() == sortPaymentState){
+                model.setRowCount(0);
+                Collections.sort(orderList, new Comparator<Current>(){
+
+                    public int compare(Current o1, Current o2) {
+                        return o1.getPaymentStatus().compareTo(o2.getPaymentStatus());
+                    }
+                });
+               
+                displayTable(orderList);
             }
 
             if(e.getSource() == edit){
