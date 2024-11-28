82d56362bdf88df10cdd2d0d980e347164b1d1e9
Jada-kay Williams <jakaywill@gmail.com>
2024-11-28 11:32:46 -0500

    File Updates.

--- Fluffy's Sweet Treats/src/CreateInventoryRecord.java
+++ Fluffy's Sweet Treats/src/CreateInventoryRecord.java
@@ -1,8 +1,161 @@
-//Author: Jada-Kay Williams
-//Last Modified:
+//Author: Jada-kay Williams
+//Last Modified: 27-11-2024
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public class CreateInventoryRecord extends JFrame {
-    //Class for the Crete Inventory GUI
+public class CreateInventoryRecord extends JFrame{
+
+    // Class for the CreateItem GUI
+
+    private JButton cancel;
+    private JButton save;
+    private JPanel itemPanel;
+    private JPanel entryPanel;
+    private JTextField enterItemName;
+    private JTextArea enterItemDesc;
+    private JTextField enterStockCount;
+    private JTextField enterAreaofStorage;
+    private JTextField entershelflife;
+    private JLabel priorityselect;
+    private JComboBox<String> priorityDrop;
+    private InventoryFile infile = new InventoryFile();
+    private String[] prioritystate= {"1", "2", "3"};
+    private String prioritystatus = " ";
+  
+
+    //Constructor formats the frame for the addition of an entry to the InventoryItems.txt file
+    public CreateInventoryRecord(){
+        setTitle("New Inventory Entry");
+
+        itemPanel = new JPanel();
+        entryPanel = new JPanel();
+
+
+
+        //Input fields for user
+
+        entryPanel.add(new JLabel("Enter item name: ")); 
+        enterItemName = new JTextField(40);
+        entryPanel.add(enterItemName);
+
+
+        entryPanel.add(new JLabel("Enter item description:"));
+        enterItemDesc = new JTextArea(3,40);
+        enterItemDesc.setLineWrap(true);
+        enterItemDesc.setWrapStyleWord(true);
+        entryPanel.add(enterItemDesc);
+
+        entryPanel.add(new JLabel("Enter stock count:"));
+        enterStockCount = new JTextField(10);
+        entryPanel.add(enterStockCount);
+
+        entryPanel.add(new JLabel("Enter item storage area:"));
+        enterAreaofStorage = new JTextField(30);
+        entryPanel.add(enterAreaofStorage);
+
+        entryPanel.add(new JLabel("Enter self life or expriration:"));
+        entershelflife = new JTextField(30);
+        entryPanel.add(entershelflife);
+
+
+        //Dropdown selection for priority status
+        priorityselect = new JLabel("Select priority status:");
+        priorityselect.setBounds(50,50,130,15);
+        priorityDrop = new JComboBox<>(prioritystate);
+        priorityDrop.setBounds(150,50,130,15);
+        entryPanel.add(priorityselect);
+        entryPanel.add(priorityDrop);
+
+        priorityDrop.addActionListener(new PriorityDropDownListener());
+
+        entryPanel.setLayout(new GridLayout(12,2));
+
+        //Save and Cancel buttons
+       
+        save= new JButton("Save");
+        cancel= new JButton("Cancel");
+
+        itemPanel.add(save);
+        itemPanel.add(cancel);
+        
+        //Add functionality for the buttons
+        cancel.addActionListener(new CloseButtonListener());
+        save.addActionListener(new SaveButtonListener());
+        
+
+        add(entryPanel, BorderLayout.CENTER);
+        add(itemPanel, BorderLayout.SOUTH);
+        pack();
+        setVisible(true);
+
+
+    }
+
+
+    //Listener for the priority status dropdown
+
+    private class PriorityDropDownListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            try{
+                prioritystatus = (String) priorityDrop.getSelectedItem();
+            }
+
+            catch(NullPointerException npe){
+                JOptionPane.showMessageDialog(CreateInventoryRecord.this, "Invalid entry detected. Please select a priority status.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+    
+    //Listener for the save button
+
+    private class SaveButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            try{
+
+                String name = enterItemName.getText().trim();
+                String description = enterItemDesc.getText().trim();
+                String stockCount = enterStockCount.getText().trim();
+                String storage = enterAreaofStorage.getText().trim();
+                String shelfLife= entershelflife.getText().trim();
+
+                Inventory item = new Inventory(0,Integer.parseInt(stockCount), Integer.parseInt(prioritystatus), name, description, storage, shelfLife);
+                
+                infile.addToInventoryFile(item);
+                
+
+                JOptionPane.showMessageDialog(CreateInventoryRecord.this,"Item Sucessfully Added", "Successful Entry",JOptionPane.INFORMATION_MESSAGE);
+                
+                setVisible(false);
+
+            }
+                      
+            catch (NullPointerException npe) {
+                JOptionPane.showMessageDialog(CreateInventoryRecord.this, "Invalid Entry Detected. Please check to ensure all fields are filled correctly.", "Error", JOptionPane.ERROR_MESSAGE);
+ 
+            }
+
+            catch(NumberFormatException nfe){
+                JOptionPane.showMessageDialog(CreateInventoryRecord.this, "Invalid Entry Detected. Please ensure numbers are entered in numerical fields.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+
+    
+        }
+    }
+
+    //Listener for the cancel button
+
+    private class CloseButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            setVisible(false);
+        }
+    }
+
 }
+
+
---
+++ Fluffy's Sweet Treats/src/InventoryFile.java
@@ -0,0 +1,209 @@
+//Author: Khanez Wallace
+//Last Modified: 27/11/24
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+
+public class InventoryFile{
+    ArrayList<Inventory>inventoryList = new ArrayList<>();
+    public InventoryFile inventFile;
+
+    public InventoryFile(){
+        this.inventFile = this;
+
+    }
+
+    //Appending inventory items to the InventoryItems.txt
+    public void addToInventoryFile(Inventory inventItem){
+        try{
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("InventoryItems.txt",true));
+
+            writer.write(inventItem.getName() + ";" + inventItem.getDescription() + ";"  + inventItem.getPriorityStatus() + ";" + inventItem.getStockCount() + ";" + inventItem.getStorage() + ";" + inventItem.getShelfLife());
+
+            writer.newLine();
+            writer.flush();
+            writer.close();
+        }
+
+        catch(IOException e){
+            JOptionPane.showMessageDialog(null, "Error saving to current order file.");
+        }
+
+    }
+    public ArrayList<Inventory> load(){
+        try(BufferedReader br = Files.newBufferedReader(Paths.get("InventoryItems.txt"))){
+            String line;
+            while ((line = br.readLine()) != null){
+                String []itemDetails = line.split(";");
+                String itName = itemDetails[1];
+                String itDesc = itemDetails[2];
+                String priorityStat = String.valueOf(itemDetails[3]);
+                String itstockCount = String.valueOf(itemDetails[4]);
+                String itStorage = itemDetails[5];
+                String itShelf = itemDetails[6];
+                
+                Inventory i = new Inventory(0,Integer.parseInt(itstockCount),Integer.parseInt(priorityStat),itName,itDesc,itStorage,itShelf);
+                inventoryList.add(i);
+            }
+
+        }
+        catch(IOException e){
+            JOptionPane.showMessageDialog(null, "File could not be accessed at this time.", "File Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return inventoryList;
+
+        
+    }
+    public ArrayList<Inventory>load2(){
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("InventoryItems.txt"))){
+        String line;
+        int ID =1;
+
+        while ((line = br.readLine()) != null){
+            String []itemDetails = line.split(";");
+            String itName = itemDetails[1];
+            String itDesc = itemDetails[2];
+            String priorityStat = String.valueOf(itemDetails[3]);
+            String itstockCount = String.valueOf(itemDetails[4]);
+            String itStorage = itemDetails[5];
+            String itShelf = itemDetails[6];
+            
+            Inventory i = new Inventory(ID,Integer.parseInt(itstockCount),Integer.parseInt(priorityStat),itName,itDesc,itStorage,itShelf);
+            inventoryList.add(i);
+            ID++;
+        }
+    }
+
+    
+    catch(IOException e){
+        JOptionPane.showMessageDialog(null, "File could not be accessed at this time.", "File Error", JOptionPane.ERROR_MESSAGE);
+    }
+    return inventoryList;
+
+
+    }
+     
+    //Allow for the deletion of inventory itens in the InventoryItems.txt file
+
+ public void deleteFromInventorytFile(int id){
+        
+        for(Inventory v: inventoryList){
+            if(v.getID() == id){
+                inventoryList.remove(v);
+                break;
+            }
+        }
+
+        try{
+            Files.deleteIfExists(Paths.get("InventoryItems.txt"));
+
+            File ifile = new File("InventoryItems.txt");
+
+            for(Inventory t: inventoryList){
+                addToInventoryFile(t);
+            }
+
+        }
+
+        
+        catch(IOException e){
+            JOptionPane.showMessageDialog(null, "File could not be accessed at this time.", "File Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    //Allow for the editing of inventory items
+    public void updateInventory(Inventory perm, String newstockCount){
+        String invent_change = perm.getName() + ";" + perm.getDescription() + ";" + String.valueOf(perm.getPriorityStatus()) + perm.getStorage() + ";" + perm.getShelfLife();
+        System.out.println(invent_change);
+
+        ArrayList<String> invtemp = new ArrayList<String>();
+
+        try{
+            FileReader fReader = new FileReader("InventoryItems.txt");
+            Scanner scanner = new Scanner(fReader);
+            String line;
+            String [] lineArray;
+
+            while(scanner.hasNext()){
+                line = scanner.nextLine();
+                if(line.length() == 0){
+                    continue;
+
+                }
+                else{
+                    lineArray = line.split(";");
+                    if(line.startsWith(invent_change)){
+                        invtemp.add(
+                            lineArray[0] + ";" +
+                            lineArray[1] + ";" +
+                            newstockCount + ";" +
+                            lineArray[3]  + ";" +
+                            lineArray[4]  + ";" +
+                            lineArray[5]  
+                        );
+                    
+
+                        
+                    }
+                    else{
+                        invtemp.add(line);
+                    }
+
+
+                    }
+                }
+                fReader.close();
+              
+              
+                System.out.println(invtemp);
+            }
+            catch(FileNotFoundException e){
+
+            }
+    
+            catch(IOException e){
+    
+            }
+    
+            try{
+                PrintWriter pWriter = new PrintWriter("InventoryItems.txt");
+                for(String str: invtemp){
+                    pWriter.println(str);
+                }
+    
+                pWriter.close();
+    
+    
+            }
+    
+            catch(Exception e){
+    
+            }
+
+
+
+            
+        }
+    }
+
+        
+    
+
+
+
+
+
